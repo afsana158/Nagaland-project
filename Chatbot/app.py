f9e3f8b3c556b9b858d5e425aa86bf24b9504d5c
Soha Afsana <sohaafsana@MacBook-Air.local>
2026-05-12 14:19:03 -0700

    CORS issue

--- Chatbot/app.py
+++ Chatbot/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 from chatbot import chatbot_response
 
 app = Flask(__name__)
+
+CORS(app, resources={
+    r"/*": {
+        "origins": "*"
+    }
+})
+
 
 @app.route("/chatbot")
 def chatbot_page():
