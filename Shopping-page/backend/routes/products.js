ee0c9f44d1f66717b1f5ee1f51a6b59740031a79
Soha Afsana <sohaafsana@MacBook-Air.local>
2026-05-12 00:45:05 -0700

    vite config

--- Shopping-page/backend/routes/products.js
+++ Shopping-page/backend/routes/products.js
@@ -27,4 +27,37 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/", (req, res) => {
+  console.log("QUERY:", req.query);
+
+  const { category, search } = req.query;
+
+  let sql = "SELECT * FROM products";
+  let values = [];
+
+  if (category) {
+    sql += " WHERE category = ?";
+    values.push(category);
+  }
+
+  if (search) {
+    sql += category ? " AND" : " WHERE";
+    sql += " name LIKE ?";
+    values.push("%" + search + "%");
+  }
+
+  console.log(sql, values);
+
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.log("DB ERROR:", err);
+      return res.status(500).json(err);
+    }
+
+    console.log("RESULT:", result);
+
+    res.json(result);
+  });
+});
+
 module.exports = router;
