c9ca84261a30e87dcaaeecb9da7b2db4f36c0196
Soha Afsana <sohaafsana@MacBook-Air.local>
2026-05-11 20:46:55 -0700

    railway deployment

--- Shopping-page/backend/db.js
+++ Shopping-page/backend/db.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root123",
-  database: "naga_shop",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 db.connect((err) => {
--- Shopping-page/backend/server.js
+++ Shopping-page/backend/server.js
@@ -16,7 +16,7 @@ app.use("/api/cart", require("./routes/cart"));
 app.use("/api/wishlist", require("./routes/wishlist"));
 app.use("/api/payment", require("./routes/payment"));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
 });
