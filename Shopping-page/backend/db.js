b505d803fc264297c7a17e209863efd8b7e6a420
Soha <afsanasoha38@gmail.com>
2026-05-16 03:45:24 -0700

    Update db.js

--- Shopping-page/backend/db.js
+++ Shopping-page/backend/db.js
@@ -1,20 +1,28 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQLPORT,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 });
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error("Database connection failed:", err);
   } else {
     console.log("Connected to MySQL");
+    connection.release();
   }
 });
 
-module.exports = db;
+module.exports = pool.promise();
