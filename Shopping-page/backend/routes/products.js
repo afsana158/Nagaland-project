b35218306dfb0fddbc533f6d9c9727a37976b87e
Soha <afsanasoha38@gmail.com>
2026-05-16 03:49:05 -0700

    Update products.js

--- Shopping-page/backend/routes/products.js
+++ Shopping-page/backend/routes/products.js
@@ -2,62 +2,39 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-router.get("/", (req, res) => {
-  const { category, search } = req.query;
+router.get("/", async (req, res) => {
+  try {
+    console.log("QUERY:", req.query);
 
-  let sql = "SELECT * FROM products";
-  let values = [];
+    const { category, search } = req.query;
 
-  if (category) {
-    sql += " WHERE category = ?";
-    values.push(category);
-  }
+    let sql = "SELECT * FROM products";
+    let values = [];
 
-  if (search) {
-    sql += category ? " AND" : " WHERE";
-    sql += " name LIKE ?";
-    values.push("%" + search + "%");
-  }
-
-  db.query(sql, values, (err, result) => {
-    if (err) {
-      return res.status(500).json(err);
+    if (category) {
+      sql += " WHERE category = ?";
+      values.push(category);
     }
-    res.json(result);
-  });
-});
 
-router.get("/", (req, res) => {
-  console.log("QUERY:", req.query);
-
-  const { category, search } = req.query;
-
-  let sql = "SELECT * FROM products";
-  let values = [];
-
-  if (category) {
-    sql += " WHERE category = ?";
-    values.push(category);
-  }
-
-  if (search) {
-    sql += category ? " AND" : " WHERE";
-    sql += " name LIKE ?";
-    values.push("%" + search + "%");
-  }
-
-  console.log(sql, values);
-
-  db.query(sql, values, (err, result) => {
-    if (err) {
-      console.log("DB ERROR:", err);
-      return res.status(500).json(err);
+    if (search) {
+      sql += category ? " AND" : " WHERE";
+      sql += " name LIKE ?";
+      values.push("%" + search + "%");
     }
+
+    console.log("SQL:", sql);
+    console.log("VALUES:", values);
+
+    const [result] = await db.query(sql, values);
 
     console.log("RESULT:", result);
 
     res.json(result);
-  });
+
+  } catch (err) {
+    console.log("DB ERROR:", err);
+    res.status(500).json({ error: "Database error" });
+  }
 });
 
 module.exports = router;
